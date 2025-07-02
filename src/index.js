5dacb2f1d09891b5af33288d1f6d1f6622fe6c86
Farshid Hossain <farshid.hossain@zorrroservices.com>
2025-07-02 18:42:41 +0530

    CORS Added

--- src/index.js
+++ src/index.js
@@ -35,7 +35,10 @@ import Razorpay from "razorpay";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ 
+  origin: ['https://kepler-22b.vercel.app/', 'http://localhost:3001', 'https://8ppzcvlk-3001.inc1.devtunnels.ms/'], 
+  credentials: true 
+}));
 
 app.use(rateLimit({
   windowMs: 15 * 60 * 1000,
