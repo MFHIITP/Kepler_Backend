97494cf288dc2da3dbabc5fa1143f07c7624282e
Farshid Hossain <farshid.hossain@zorrroservices.com>
2025-07-15 23:58:50 +0530

    added docker with single container

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:18
+WORKDIR /app
+COPY package*.json ./
+
+RUN npm install -g nodemon
+
+RUN npm install
+COPY . .
+EXPOSE 8000
+CMD [ "npm", "run", "start" ]
--- src/utils/connection.utils.js
+++ src/utils/connection.utils.js
@@ -5,9 +5,12 @@ import logger from "./winston_logger.js";
 
 const server = process.env.SERVER;
 const database = process.env.DATABASE
+
 const connect = async () => {
     try {
-      await mongoose.connect(`mongodb+srv://${server}/${database}`);
+      const url = `mongodb+srv://${server}/${database}`;
+      // const url = ``
+      await mongoose.connect(url);
       logger.info('Database Successfully Connected')
       console.log("Connecion Successful");
     } catch (err) {
