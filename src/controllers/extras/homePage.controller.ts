809f92531cb9d124bea186d05f2fac9c61e74264
Farshid Hossain <farshid.hossain@zorrroservices.com>
2025-08-17 23:28:08 +0530

    minor changes

--- src/controllers/extras/homePage.controller.ts
+++ src/controllers/extras/homePage.controller.ts
@@ -23,13 +23,15 @@ const homePage = async(req:Request, res:Response) => {
         }
         const ipRaw = req.headers["x-forwarded-for"];
         const ip = ipRaw?.split(",")[0].trim();
-        const info = await axios.get(`https://ipapi.co/${ip}/json`) as IPInterface;
+        const info = await axios.get(`https://ipinfo.io/${ip}/json?token=c13532365e8939`)
+        console.log(info)
         const query = `INSERT INTO visits (ip_address, country, city, user_agent) values ($1, $2, $3, $4);`;
-        await pool.query(query, [ip, info.country_name, info.city, null])
+        await pool.query(query, [ip, info.data.country_name, info.data.city, null])
+
         console.log("User entered successfully");
     }
     catch (error) {
-        console.log("Error occurred while entering user.");
+        console.log("Error occurred while entering user.", error);
     }
 }
 export default homePage
