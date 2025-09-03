6888c85da752f5fccf6824b032974623858e4a23
Farshid Hossain <farshid.hossain@zorrroservices.com>
2025-09-03 13:51:02 +0530

    slight modification

--- src/Problems/getAllProblems.controller.ts
+++ src/Problems/getAllProblems.controller.ts
@@ -25,9 +25,9 @@ const getAllProblems = async(req:Request, res:Response) => {
         })
         const dailyProblem = modifiedProblems.find(problem  => problem.id == new Date().getDate());
         const codingDetails = await codingModel.find({email: email})
-        const streakValue = codingDetails[0].streak || 0;
-        const keplerBits = codingDetails[0].keplerBits || 0;
-        const solved = codingDetails[0].numberSolved || 0;
+        const streakValue = codingDetails[0]?.streak || 0;
+        const keplerBits = codingDetails[0]?.keplerBits || 0;
+        const solved = codingDetails[0]?.numberSolved || 0;
         res.status(200).json({
             problemList: modifiedProblems,
             dailyProblem: dailyProblem,
