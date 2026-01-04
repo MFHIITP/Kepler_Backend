fe706a5caccb23f957adaabca171e6e056ff60b0
Farshid Hossain <farshid.hossain@zorrroservices.com>
2026-01-04 23:33:58 +0530

    minor changes

--- src/controllers/allCourses.controller.ts
+++ src/controllers/allCourses.controller.ts
@@ -3,7 +3,7 @@ import { grouplist } from "../local_dbs";
 
 const getAllCourses = async (request: Request, res: Response) => {
   const courses = grouplist.filter(data => data.course === true && data.name != 'Community Group').map(data => ({
-    name: data.name.replace(/^Computer Science\s*/, ""),
+    name: data.name.replace(/^Computer Science - \s*/, ""),
     exam: data.exam,
     courseDescription: data.courseDescription,
     features: data.features,
