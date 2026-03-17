54073234526564d7fd7c4bf7fdb75321a62e8947
MFHIITP <hossainfarshid@gmail.com>
2026-03-17 23:21:06 +0530

    ensured a student is added to more than one google group

--- src/GoogleGroups/GoogleGroupCallback.controller.ts
+++ src/GoogleGroups/GoogleGroupCallback.controller.ts
@@ -10,21 +10,21 @@ const getGoogleGroupRefreshToken = async(req: Request, res: Response) => {
     }
 
     const { tokens } = await OAuth2Client_GoogleGroups.getToken(code);
-    OAuth2Client_GoogleGroups.setCredentials(tokens);
+    // OAuth2Client_GoogleGroups.setCredentials(tokens);
 
     const oauth2 = google.oauth2({
         auth: OAuth2Client_GoogleGroups,
         version: "v2"
     });
 
-    const userInfo = await oauth2.userinfo.get();
+    // const userInfo = await oauth2.userinfo.get();
 
-    if(userInfo.data.email != "hossainfarshid@gmail.com" && userInfo.data.email != "kepler.xxiib.cygnus@gmail.com" && userInfo.data.email != "kepler@keplercodes.com" && userInfo.data.email != "farshidhossain@keplercodes.com"){
-        res.status(401).json({
-            message: "UnAuthorized"
-        })
-        return;
-    }
+    // if(userInfo.data.email != "hossainfarshid@gmail.com" && userInfo.data.email != "kepler.xxiib.cygnus@gmail.com" && userInfo.data.email != "kepler@keplercodes.com" && userInfo.data.email != "farshidhossain@keplercodes.com"){
+    //     res.status(401).json({
+    //         message: "UnAuthorized"
+    //     })
+    //     return;
+    // }
 
     console.log("Tokens received ", tokens);
     res.send(`
--- src/postgresModels/GoogleGroupsStudentSchema/createGoogleGroupStudentSchema.postgres.ts
+++ src/postgresModels/GoogleGroupsStudentSchema/createGoogleGroupStudentSchema.postgres.ts
@@ -3,7 +3,7 @@ import pool from "../../utils/postgresConnection.utils.js";
 (async() => {
     const query = `CREATE TABLE IF NOT EXISTS googlegroupsstudentslist (
         id SERIAL PRIMARY KEY,
-        student_email VARCHAR(255) NOT NULL UNIQUE,
+        student_email VARCHAR(255) NOT NULL,
         group_email VARCHAR(255) NOT NULL,
         expiry_date DATE NOT NULL
     )`;
